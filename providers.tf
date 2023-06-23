4f4ad507edb593e587efffba1c555d85d0472841
Julian Doyle <doylej101@gmail.com>
2023-06-23 10:18:54 +0100

    Reorg

--- providers.tf
+++ providers.tf
@@ -4,28 +4,25 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
-/*  
+
     azurerm = {
       source = "hashicorp/azurerm"
       # Root module should specify the maximum provider version
       # The ~> operator is a convenient shorthand for allowing only patch releases within a specific minor release.
       version = "~> 3.0"
     }
-    default_tags {
-        tags = {
-            Terraformed = "True"
-        }
-    }
+
   }
-*/  
 }
-}
+
 
 # Configure the AWS Provider
 provider "aws" {
   region  = "eu-west-2"
   }
-/*
+
 provider "azurerm" {
-*/
+  features {}
+}
+
 
