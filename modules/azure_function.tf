289d3033e7bba22281c4edf42343ca82ec2001d8
Julian Doyle <doylej101@gmail.com>
2023-06-23 14:25:46 +0100

    Reorg

--- modules/azure_function.tf
+++ modules/azure_function.tf
@@ -26,7 +26,7 @@ resource "azurerm_linux_function_app" "example" {
 
 #  storage_account_name       = azurerm_storage_account.example.name
 #  storage_account_access_key = azurerm_storage_account.example.primary_access_key
-#  service_plan_id            = azurerm_service_plan.example.id
+  service_plan_id            = azurerm_service_plan.example.id
 
 #  site_config {}
 }
