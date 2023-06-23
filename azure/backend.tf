4bb32b5cae6bc708dcabb9439769b81bca4fdf5d
Julian Doyle <doylej101@gmail.com>
2023-06-23 12:32:09 +0100

    Reorg

--- azure/backend.tf
+++ azure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "hackathon-rg"
-    storage_account_name = "hackathonsajulianddd  "
+    storage_account_name = "hackathonsajulianddd"
     container_name       = "julianddd-tfstate"
     key                  = "julianddd-tfstate"
   }
